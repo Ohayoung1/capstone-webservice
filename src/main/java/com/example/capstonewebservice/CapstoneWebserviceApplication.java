19e73479bfcedaf1f975be9b4d0ebeb9e70ef219
Ohayoung1 <dhgkdud114@naver.com>
2023-09-23 20:33:48 +0900

    Initial commit

---
+++ src/main/java/com/example/capstonewebservice/CapstoneWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstonewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneWebserviceApplication.class, args);
+	}
+
+}
